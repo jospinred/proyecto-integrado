3a53d706af12245f214f6e66dbc553816038fe2f
jospinred <jpinred062@g.educaand.es>
2024-03-06 10:08:04 +0100

    first commit

---
+++ Backend/proyecto-integrado/src/main/java/com/velazquez/proyectointegrado/ProyectoIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.velazquez.proyectointegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntegradoApplication.class, args);
+    }
+
+}
